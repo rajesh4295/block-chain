59dbffea30a60675d2d5550e8c7a1b882a95373e
Rakesh Kumar <rakeshtokumar@gmail.com>
2020-01-19 20:42:33 +0530

    Added test and gitlab yml

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var mocha = require('mocha');
+var chai = require('chai');
+
+describe("Test",function(){
+    it("Test one",function(done){
+        done();
+    });
+
+    it("Test two",function(done){
+        done();
+    });
+})
